ca1b53cc07aa8286cae197f08d1437ffda8367cd
zaczh <zaczh@hotmail.com>
2017-03-08 14:15:51 +0800

    add parameter nullability info

--- ObjcJieba/ObjcJiebaKeywordExtractor.h
+++ ObjcJieba/ObjcJiebaKeywordExtractor.h
@@ -8,16 +8,16 @@
 
 #import <Foundation/Foundation.h>
 @interface ObjcJiebaKeywordExtractorResult: NSObject
-@property (nonatomic, copy) NSString *word;
+@property (nonatomic, copy) NSString * _Nonnull word;
 @property (nonatomic) NSRange range;
 @property (nonatomic) double weight;
 @end
 
 @interface ObjcJiebaKeywordExtractor : NSObject
-- (instancetype)initWithDictPath: (NSString *)dicpath
-                     hmmFilePath: (NSString *)hmmFilePath
-                         idfPath: (NSString *)idfPath
-                    stopWordPath: (NSString *)stopWordPath
-                        userDict: (NSString *)userDict;
-- (void)extract:(NSString *)str completion:(void (^)(NSArray<ObjcJiebaKeywordExtractorResult *>*, NSError *))completion;
+- (instancetype _Nonnull)initWithDictPath: (NSString * _Nullable)dicpath
+                     hmmFilePath: (NSString * _Nullable)hmmFilePath
+                         idfPath: (NSString * _Nullable)idfPath
+                    stopWordPath: (NSString * _Nullable)stopWordPath
+                        userDict: (NSString * _Nullable)userDict;
+- (void)extract:(NSString * _Nonnull)str completion:(void (^ _Nullable)(NSArray<ObjcJiebaKeywordExtractorResult *>* _Nullable results, NSError *_Nullable error))completion;
 @end
